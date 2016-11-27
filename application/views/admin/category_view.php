327c541309a8481d26d72f02168a3c0e079553c4
stworchwk <satawat535@gmail.com>
2016-11-27 13:52:12 +0700

    เพิ่มหน้า Category_view และแก้ไข

--- application/views/admin/category_view.php
+++ application/views/admin/category_view.php
@@ -5,6 +5,32 @@
             <h1 class="page-header">จัดการหมวดหมู่ข่าว</h1>
         </div>
         <!-- /.col-lg-12 -->
+        <div class="col-lg-12">
+
+            <a href="#" class="btn btn-xs btn-primary">
+                <i class="fa fa-plus"></i> เพิ่มหมวดหมู่
+            </a>
+
+            <table class="table">
+                <tr>
+                    <th>#</th>
+                    <th>ชื่อหมวดหมู่</th>
+                    <th>จัดการ</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>กีฬา</td>
+                    <td>
+                        <a href="#" class="btn btn-xs btn-warning">
+                            <i class="fa fa-edit"></i> แก้ไข
+                        </a>
+                        <a href="#" class="btn btn-xs btn-danger">
+                            <i class="fa fa-times"></i> ลบ
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
     <!-- /.row -->
 </div>
